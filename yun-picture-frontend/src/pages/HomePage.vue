4e76bd0297a01bc29e7bc720f0184f48fe4a5ffd
ztc123-ok <2844692623@qq.com>
2026-02-15 17:52:28 +0800

    前端项目初始化

---
+++ yun-picture-frontend/src/pages/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="homePage">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+#homePage {
+  margin-bottom: 16px;
+}
+
+#homePage .search-bar {
+  max-width: 480px;
+  margin: 0 auto 16px;
+}
+
+#homePage .tag-bar {
+  margin-bottom: 16px;
+}
+</style>
---
+++ yun-picture-frontend/src/stores/useLoginUserStore.ts
@@ -0,0 +1,29 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+/**
+ * 存储登录用户信息的状态
+ */
+export const useLoginUserStore = defineStore('loginUser', () => {
+  const loginUser = ref<any>({
+    userName: '未登录',
+  })
+
+  /**
+   * 远程获取登录用户信息
+   */
+  async function fetchLoginUser() {
+
+  }
+
+  /**
+   * 设置登录用户
+   * @param newLoginUser
+   */
+  function setLoginUser(newLoginUser: any) {
+    loginUser.value = newLoginUser
+  }
+
+  // 返回
+  return { loginUser, fetchLoginUser, setLoginUser }
+})
