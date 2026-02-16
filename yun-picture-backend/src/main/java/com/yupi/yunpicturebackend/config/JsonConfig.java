b154ccc733a179a04746829ed4db697264cd36d3
ztc123-ok <2844692623@qq.com>
2026-02-16 20:46:32 +0800

    用户增删改查

--- yun-picture-backend/src/main/java/com/yupi/yunpicturebackend/config/JsonConfig.java
+++ yun-picture-backend/src/main/java/com/yupi/yunpicturebackend/config/JsonConfig.java
@@ -16,13 +16,13 @@ public class JsonConfig {
     /**
      * 添加 Long 转 json 精度丢失的配置
      */
-//    @Bean
-//    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
-//        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-//        SimpleModule module = new SimpleModule();
-//        module.addSerializer(Long.class, ToStringSerializer.instance);
-//        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
-//        objectMapper.registerModule(module);
-//        return objectMapper;
-//    }
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        return objectMapper;
+    }
 }
