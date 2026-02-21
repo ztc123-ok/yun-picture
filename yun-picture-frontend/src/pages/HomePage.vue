dc17aca87a0bd596c3d6f494ba080587845f41bf
ztc123-ok <2844692623@qq.com>
2026-02-21 12:32:36 +0800

    前端图片主页搜索

--- yun-picture-frontend/src/pages/HomePage.vue
+++ yun-picture-frontend/src/pages/HomePage.vue
@@ -1,19 +1,52 @@
 <template>
   <div id="homePage">
+    <!-- 搜索框-->
+    <div class="search-bar">
+      <a-input-search
+        v-model:value="searchParams.searchText"
+        placeholder="从海量图片中搜索"
+        enter-button="搜索"
+        size="large"
+        @search="doSearch"
+      />
+    </div>
+    <!-- 分类和标签筛选-->
+    <a-tabs v-model:active-key="selectedCategory" @change="doSearch">
+      <a-tab-pane key="all" tab="全部" />
+      <a-tab-pane v-for="category in categoryList" :tab="category" :key="category" />
+    </a-tabs>
+    <div class="tag-bar">
+      <span style="margin-right: 8px">标签：</span>
+      <a-space :size="[0, 8]" wrap>
+        <a-checkable-tag
+          v-for="(tag, index) in tagList"
+          :key="tag"
+          v-model:checked="selectedTagList[index]"
+          @change="doSearch"
+        >
+          {{ tag }}
+        </a-checkable-tag>
+      </a-space>
+    </div>
+    <!-- 图片列表-->
     <a-list
-      :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl:6 }"
+      :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }"
       :data-source="dataList"
       :pagination="pagination"
       :loading="loading"
     >
       <template #renderItem="{ item: picture }">
-        <a-list-item>
+        <a-list-item style="padding: 0">
           <!--单张图片-->
-          <a-card hoverable>
+          <a-card hoverable @click="doClickPicture(picture)">
             <template #cover>
-              <img :alt="picture.name" :src="picture.url" style="height: 180px ; object-fit: cover"/>
+              <img
+                :alt="picture.name"
+                :src="picture.url"
+                style="height: 180px; object-fit: cover"
+              />
             </template>
-            <a-card-meta title="picture.name">
+            <a-card-meta :title="picture.name">
               <template #description>
                 <a-flex>
                   <a-tag color="green">
@@ -34,8 +67,12 @@
 
 <script setup lang="ts">
 import { computed, onMounted, reactive, ref } from 'vue'
-import { listPictureVoByPageUsingPost } from '@/api/pictureController'
+import {
+  listPictureTagCategoryUsingGet,
+  listPictureVoByPageUsingPost,
+} from '@/api/pictureController'
 import { message } from 'ant-design-vue'
+import {useRoute, useRouter} from "vue-router";
 
 // 定义数据
 const dataList = ref<API.PictureVO[]>([])
@@ -52,17 +89,29 @@ const searchParams = reactive<API.PictureQueryRequest>({
 
 // 获取数据
 const fetchData = async () => {
-  loading.value = true;
-  const res = await listPictureVoByPageUsingPost({
+  loading.value = true
+  // 转换搜索参数
+  const params = {
     ...searchParams,
+    tags: [] as string[],
+  }
+  if (selectedCategory.value !== 'all') {
+    params.category = selectedCategory.value
+  }
+  // [true, false, false] => ['java']
+  selectedTagList.value.forEach((useTag, index) => {
+    if (useTag) {
+      params.tags.push(tagList.value[index])
+    }
   })
+  const res = await listPictureVoByPageUsingPost(params)
   if (res.data.code === 0 && res.data.data) {
     dataList.value = res.data.data.records ?? []
     total.value = res.data.data.total ?? 0
   } else {
     message.error('获取数据失败，' + res.data.message)
   }
-  loading.value = false;
+  loading.value = false
 }
 
 // 页面加载时获取数据，请求一次
@@ -83,6 +132,45 @@ const pagination = computed(() => {
       fetchData()
     },
   }
+})
+
+// 搜索
+const doSearch = () => {
+  // 重置搜索条件
+  searchParams.current = 1
+  fetchData()
+}
+
+// 标签分类列表
+const categoryList = ref<string[]>([])
+const tagList = ref<string[]>([])
+const selectedCategory = ref<string>('all')
+const selectedTagList = ref<boolean[]>([])
+
+/**
+ * 获取标签和分类选项
+ * @param values
+ */
+const getTagCategoryOptions = async () => {
+  const res = await listPictureTagCategoryUsingGet()
+  if (res.data.code === 0 && res.data.data) {
+    tagList.value = res.data.data.tagList ?? []
+    categoryList.value = res.data.data.categoryList ?? []
+  } else {
+    message.error('获取标签分类列表失败，' + res.data.message)
+  }
+}
+
+const router = useRouter()
+
+const doClickPicture = (picture: API.PictureVO) => {
+  router.push({
+    path: `/picture/${picture.id}`,
+  })
+}
+
+onMounted(() => {
+  getTagCategoryOptions()
 })
 </script>
 
